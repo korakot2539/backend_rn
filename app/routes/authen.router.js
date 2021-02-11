5ea68681b5e9854fafdc2c019b43498fc76016ad
Korakot <58419807+korakot2539@users.noreply.github.com>
2021-02-11 22:41:56 +0700

    edit login API

--- app/routes/authen.router.js
+++ app/routes/authen.router.js
@@ -1,28 +1,32 @@
 var express = require("express");
 var router = express.Router();
-var Admin = require("../models/admin.model");
+// var Admin = require("../models/admin.model");
 var Member = require("../models/member.model");
 
 // ADMIN LOGIN
-router.post("/adminlogin", (req, res) => {
-  var {USERNAME,PASSWORD} = new Admin(req.body);
-  Admin.findOne({USERNAME}).exec((err, data) => {
-    // console.log(data.PASSWORD);
-    if (data) {
-      if(data.PASSWORD===PASSWORD){
-        return res.status(200).send(data);
-      }
-      return res.status(404).send("Username or Password incorrect.");
-    }else{
-      return res.status(404).send("Username does not exist.");
-    }
-  });
-});
+// router.post("/adminlogin", (req, res) => {
+//   var {USERNAME,PASSWORD} = new Admin(req.body);
+//   Admin.findOne({USERNAME}).exec((err, data) => {
+//     // console.log(data.PASSWORD);
+//     if (data) {
+//       if(data.PASSWORD===PASSWORD){
+//         return res.status(200).send(data);
+//       }
+//       return res.status(404).send("Username or Password incorrect.");
+//     }else{
+//       return res.status(404).send("Username does not exist.");
+//     }
+//   });
+// });
 
 // LOGIN
 router.post("/login", (req, res) => {
-  var {UID,IDNUMBER} = new Member(req.body);
+  var UID = req.body.UID;
+  var IDNUMBER = req.body.IDNUMBER;
+
   Member.findOne({UID}).exec((err, data) => {
+    // var json = {"dataID":data.IDNUMBER,"ID":IDNUMBER};
+    // return res.status(200).send(json);
     if (data) {
       if(data.IDNUMBER===IDNUMBER){
         return res.status(200).send(data);
