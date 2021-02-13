78b09450a87105b6fd90fbf3be2c4fdf85ff384c
Korakot <58419807+korakot2539@users.noreply.github.com>
2021-02-13 21:44:16 +0700

    Update queue

--- app/models/queue.model.js
+++ app/models/queue.model.js
@@ -1,13 +1,29 @@
 var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-var queueSchema = mongoose.Schema(
+var MEMBER = require("./member.model");
+var LIST = require("./list.model");
+
+var queueSchema = Schema(
   {
     // กำหนด ชื่อและชนิดของ document เรา
-    QUEUE: {type: String},
-    UID: {type: String},
-    DATETIME: {type: String},
-    LIST_ID: {type: String},
-    ADMIN_ID: {type: String},
+    QUEUE: String,
+    UID: String,
+    DATETIME: String,
+    LIST_ID: String,
+    ADMIN_ID: String,
+    USERDETAIL:{
+        type: Schema.Types.ObjectId, 
+        ref:"MEMBER"
+    },
+    ADMINDETAIL:{
+      type: Schema.Types.ObjectId,
+      ref:"MEMBER"
+    },
+    LISTDETAIL:{
+      type: Schema.Types.ObjectId,
+      ref:"LIST"
+    }
   },
   {
     // กำหนด collection ของ MongoDB หรือจะไม่กำหนดก็ได้
@@ -17,4 +33,6 @@ var queueSchema = mongoose.Schema(
 
 // ถ้าไม่ได้กำหนด collection ข้างบน default จะเป็น "MEMBER"
 var QUEUE = mongoose.model("QUEUE", queueSchema);
+
 module.exports = QUEUE;
+
